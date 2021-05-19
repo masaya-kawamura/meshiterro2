9b31576ebfa09c9903ab788e3fc080e136fba44b
masaya-kawamura <lucky.number0128@gmail.com>
2021-05-19 10:44:49 +0000

    [Update]favorite Ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,19 +4,17 @@ class FavoritesController < ApplicationController
   # post_image_favorites POST
   # /post_images/:post_image_id/favorites(.:format)
   def create
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_user.favorites.new(post_image_id: post_image.id)
+    @post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.new(post_image_id: @post_image.id)
     favorite.save
-    redirect_to post_image_path(post_image)
   end
 
   # post_image_favorites DELETE
   # /post_images/:post_image_id/favorites(.:format)
   def destroy
-    post_image = PostImage.find(params[:post_image_id])
-    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
+    @post_image = PostImage.find(params[:post_image_id])
+    favorite = current_user.favorites.find_by(post_image_id: @post_image.id)
     favorite.destroy
-    redirect_to post_image_path(post_image)
   end
 
 end
